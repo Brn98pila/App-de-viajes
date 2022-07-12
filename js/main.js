d760212bab4f6a6aac9cff828affb8ebf819a14e
Brunno-notebook <brusil21@hotmail.com>
2022-07-11 21:57:54 -0300

    9no commit, agregado de html, terminando pestana inicial

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+//BOTON PARA CAMBIAR DE LADO LA SALIDA Y LA LLEGADA
+function cambiarDeLadoBotonesBox(){
+    let browserCaja = document.getElementById('browser-box');
+    browserCaja.className = "browser-caja-hijos";
+}
+
+
+
 // TODO ESTO ES PARA HACER TECLAS DE SUBIR Y BAJAR CANTIDADES
 let numeroPersonas = 1;
 let numeroMenores = 0;
 let numeroBebes = 0;
-
-
-
 
 
 function masPersonas(){
@@ -13,6 +18,8 @@ function masPersonas(){
     if ((numeroPersonas > 0)&&(numeroPersonas < 8)){
         let down = document.getElementById("down");
         down.style.color = '#5383ec';
+        down.style.pointerEvents = 'auto'
+        
     }
     else if (numeroPersonas == 9){
         let up = document.getElementById("up");
@@ -32,7 +39,7 @@ function menosPersonas(){
         down.style.color = '#E6EAEF';
         down.style.pointerEvents = 'none'
     }
-    else if ((numeroPersonas < 10)||(numeroPersonas >= 0)){
+    else if ((numeroPersonas < 10)||(numeroPersonas > 0)){
         down.style.pointerEvents = 'auto';
         let up = document.getElementById("up");
         up.style.pointerEvents = "auto";
@@ -46,6 +53,8 @@ function masMenores(){
     if ((numeroMenores > 0)&&(numeroMenores < 8)){
         let downB = document.getElementById("downb");
         downB.style.color = '#5383ec';
+        downB.style.pointerEvents = 'auto'
+        
     }
     else if (numeroMenores == 9){
         let upB = document.getElementById("upb");
@@ -76,6 +85,8 @@ function masBebes(){
     if ((numeroBebes > 0)&&(numeroBebes < 8)){
         let downC = document.getElementById("downc");
         downC.style.color = '#5383ec';
+        downC.style.pointerEvents = 'auto'
+
     }
     else if (numeroBebes == 9){
         let upC = document.getElementById("upc");
@@ -100,63 +111,81 @@ document.getElementById("number-bebes").textContent = numeroBebes;
 }
 }
 
-//FUNCION BOTON SUMA DE LA CANTIDAD DE PERSONAS
+// BOTONES DE CLASE PARA VIAJAR EN AVION
+
+let botonEconomica = document.getElementById('boton-clase-economica') ;
+let botonEjecutiva = document.getElementById('boton-clase-ejecutiva') ;
+
+function SeleccionBotonClaseEconomica(){
+    botonEjecutiva.classList.remove('boton-active');
+    botonEconomica.className = 'boton-active'
+    document.getElementById('clase-box').innerHTML = 'Economica'
+} 
+function SeleccionBotonClaseEjecutiva(){
+    botonEconomica.classList.remove('boton-active');
+    botonEjecutiva.className = 'boton-active'
+    document.getElementById('clase-box').innerHTML = 'Ejecutiva';
+
+}
+
+//FUNCION BOTON SUMA DE LA CANTIDAD DE PERSONAS Y BLOQUEA LOS BOTONES AL SUMAR TODOS 9
 
 function botonSumaDePasajeros(){
     let totalAdultos = document.getElementById("number-person").textContent;
     let totalMenores = document.getElementById("number-menores").textContent;
     let totalBebes = document.getElementById("number-bebes").textContent;
     
-    let numeroTotalPasajeros = parseInt(totalAdultos) + parseInt(totalMenores) + parseInt (totalBebes);
-
-    
+    globalThis.numeroTotalPasajeros = parseInt(totalAdultos) + parseInt(totalMenores) + parseInt (totalBebes);
     document.getElementById('cantidad-pasajeros').innerHTML = numeroTotalPasajeros +' Pasajeros';
 
+    document.getElementById('cerrar-offcanvas-pasajeros').click();
+
+    let conjuntoBotonesSuma = [document.getElementById("upc"),document.getElementById("upb"),document.getElementById("up")]
 
     
-        console.log(numeroTotalPasajeros);
-}
-
-// BOTONES DE CLASE
-    let botonEconomica = document.getElementById('boton-clase-economica') ;
-    let botonEjecutiva = document.getElementById('boton-clase-ejecutiva') ;
-
-    function SeleccionBotonClaseEconomica(){
-        botonEjecutiva.classList.remove('boton-active');
-        botonEconomica.className = 'boton-active'
-    } 
-    function SeleccionBotonClaseEjecutiva(){
-        botonEconomica.classList.remove('boton-active');
-        botonEjecutiva.className = 'boton-active'
-    } 
-
-
-// Array para offcanvas destino
-
-const ciudades = ['Rio de Janeiro', 'Londres','Atenes','New York','Amsterdam','Bogota','Roma','Playa del Carmen','Santiago de Chile','Mar del Plata']
-const paises = ["Alemania","Argentina","Australia","Bélgica","Bolivia","Brasil","Canadá","Chile","China","Colombia",
-"Cuba","Dinamarca","Ecuador","Egipto","El Salvador","Eslovenia","España","Estados Unidos",
-"Finlandia","Francia","Grecia","Guatemala","Honduras","Hungría","India","Irak",
-"Irán","Irlanda","Islandia","Israel","Italia","Jamaica","Japón","México",
-"Mongolia","Nicaragua","Nigeria","Noruega","Nueva Zelanda","Países Bajos","Paraguay","Perú",
-"Polonia","Portugal","Reino Unido","República Dominicana","Rusia","Samoa","Senegal","Sudáfrica",
-"Suecia","Suiza","Tailandia","Tanzania","Turquía","Ucrania","Uganda","Uruguay",
-"Uzbekistán","Vanuatu","Venezuela","Vietnam","Yemen","Yibuti","Zambia","Zimbabue"]
-
-
-const Argentina = {
+    /* if(numeroTotalPasajeros >= 9){                           Todavia no pude encontrar la manera de ejectuarlo
+        conjuntoBotonesSuma.forEach(boton =>{                   Lo entiendo como, si la suma del numero total de pasajeros
+            boton.style.color('#fff');                          es mayor o igual a 9, se ejecuta el bucle que cambia 
+            boton.style.pointerEvents('none');                  los atributos de cada boton para tener un maximo de 9 pasajeros, 
+        })                                           asi no pueden enviar  mas de 9 pasajeros. Espero a la clase para investigarlo mejor
+                                                                 
+    }   */                                                       
+   
 
 }
+
+
+
+
+// Array para offcanvas destino / viaje en avion
+function constructorPaises(){
+   this.ciudad = constructorPaises.ciudad;
+   this.acronimo = constructorPaises.acronimo;
+   this.icono = constructorPaises.icono;
+   this.pais = constructorPaises.pais;
+}
+const grecia = new constructorPaises("Atenas","GRE","icon-avion","Grecia");
+const inglaterra = new constructorPaises("Londres","ING","icon-avion","Inglaterra");
+const estadosUnidos = new constructorPaises("NewYork","US","icon-avion","Estados Unidos");
+const colombia = new constructorPaises("NewYork","US","icon-avion","Colombia");
+const paisesBajos = new constructorPaises("Amsterdam","HOL","icon-avion","Paises Bajos");
+const italia = new constructorPaises("Roma","ITA","icon-avion","Italia");
+const mexico = new constructorPaises("Playa del Carmen","MEX","icon-avion","Mexico");
+const argentina = new constructorPaises("Bariloche","ARG","icon-avion","Argentina");
+const japon = new constructorPaises("Tokio","JPN","icon-avion","Japon");
+const brasil = new constructorPaises("Rio de Janeiro","BRS","icon-avion","Brasil");
+
+const paises = [grecia,inglaterra,estadosUnidos,colombia,paisesBajos,italia,mexico,argentina,japon,brasil]
 
 let cantidadDestinos = ""
 
 
 function Destinos(){
-    for (i=0; i<67;i++){
+    for (i=0; i<10;i++){
         const container = document.getElementsByClassName('destinos-container');
         const hijo = document.createElement('div');
         hijo.className = 'destino'
-        hijo.innerHTML = "<img></img><span>" + cantidadDestinos + "</span>"
+        hijo.innerHTML = "<i class='" + paises.icono + "'></i><div><span>" + paises.ciudad, paises.pais + "</span><span><p>Todos los aeropuertos</p></span></div><span class='acronimo'>" + paises.acronimo + "</span>"
 
         container[0].appendChild(hijo);
         cantidadDestinos = paises[i] 
